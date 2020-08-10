9f0fcc79723ccc5a56799bacec441bf4dcf2d7e8
Mootooly <61703591+Mootooly@users.noreply.github.com>
2020-08-10 18:19:33 -0500

    Add files via upload

---
+++ Items/BottomlessBucket.cs
@@ -0,0 +1,59 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.Localization;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace BetterBottomlessBuckets.Items
+{
+	public class BottomlessBucket : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			// DisplayName.SetDefault("BottomlessHoneyBucket"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("+2 range\nContains an endless amount of nothing");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 11;
+			item.useAnimation = 11;
+			item.useStyle = 1;
+			item.value = 500000;
+			item.rare = 7;
+			item.autoReuse = true;
+		}
+        
+        public override bool UseItem(Player player)
+		{
+            if(Math.Abs((player.position.X/16) - Player.tileTargetX) > Player.tileRangeX + 2 || Math.Abs((player.position.Y/16) - Player.tileTargetY) > Player.tileRangeY + 2){
+                return false;
+            }
+            Main.PlaySound(19, (int) player.position.X, (int) player.position.Y, 1, 1f, 0.0f);
+            Main.tile[Player.tileTargetX, Player.tileTargetY].liquid = 0;
+            WorldGen.SquareTileFrame(Player.tileTargetX, Player.tileTargetY, true);
+            if (Main.netMode == 1)
+            NetMessage.sendWater(Player.tileTargetX, Player.tileTargetY);
+            return true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DemoniteBar, 20);
+			recipe.AddIngredient(ItemID.MeteoriteBar, 20);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+
+			ModRecipe recipe2 = new ModRecipe(mod);
+			recipe.AddIngredient(mod, "BottomlessBucket", 1);
+			recipe.needWater = true;
+			recipe.SetResult(ItemID.BottomlessBucket);
+			recipe.AddRecipe();
+		}
+	}
+}
